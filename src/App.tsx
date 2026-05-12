9f3214329ff54aa2d2921be4b6f69bb8eda32e12
Heinrich <heinrich9383@gmail.com>
2026-05-12 14:34:20 +0700

    added docker and github action however i haven't test it yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Link, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Home,NotFound,Events,Dashboard } from './pages'
 import { Navigationbar } from './components'
